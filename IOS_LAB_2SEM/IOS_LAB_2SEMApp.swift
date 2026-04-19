f2b16e33fa14f9ea1f25699222da0667ebaca524
krnklvx <sawosit@gmail.com>
2026-04-19 13:40:18 +0300

    Initial Commit

---
+++ IOS_LAB_2SEM/IOS_LAB_2SEMApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_LAB_2SEMApp.swift
+//  IOS_LAB_2SEM
+//
+//  Created by krnklvx on 03.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_LAB_2SEMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
